1316664e80c62677389103b511425743ce69e58e
naingolan <kelvinsdechaw@gmail.com>
2023-04-01 02:48:24 +0300

    first commit

---
+++ LittleLemonDRF/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+from .models import *
+from .serializers import MenuItemsSerializer
+from rest_framework import generics
+
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemsSerializer
+    
+class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemsSerializer
